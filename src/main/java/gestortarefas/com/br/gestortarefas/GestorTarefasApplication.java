0391e28ce6f96f7cdac85f8d18a19979e451231b
João Pedro <aguiarhernandez4@gmail.com>
2026-01-25 16:44:08 -0300

    Push Inicial

---
+++ src/main/java/gestortarefas/com/br/gestortarefas/GestorTarefasApplication.java
@@ -0,0 +1,13 @@
+package gestortarefas.com.br.gestortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorTarefasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorTarefasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gestortarefas/com/br/gestortarefas/GestorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package gestortarefas.com.br.gestortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorTarefasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
