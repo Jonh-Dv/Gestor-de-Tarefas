13e35ba84e92016bca495eb5678e7887beba6244
João Pedro <joaopedro192003@gmail.com>
2026-01-25 18:13:51 -0300

    Adicionado Entidade Tarefa e arquivo controller vazio

---
+++ src/main/java/gestortarefas/com/br/gestortarefas/controller/TarefaController.java
@@ -0,0 +1,10 @@
+package gestortarefas.com.br.gestortarefas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tarefas")
+public class TarefaController {
+
+}
---
+++ src/main/java/gestortarefas/com/br/gestortarefas/entity/Tarefa.java
@@ -0,0 +1,42 @@
+package gestortarefas.com.br.gestortarefas.entity;
+
+import jakarta.persistence.Column;;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "TAREFA")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_TAREFA")
+    private int idTarefa;
+
+    @Column(name = "NOME_TAREFA", nullable = false, length = 100)
+    private String nomeTarefa;
+
+    @Column(name = "DESCRICAO_TAREFA", length = 255)
+    private String descricaoTarefa;
+
+    @Column(name = "STATUS_TAREFA")
+    private String statusTarefa;
+
+    @Column(name = "PRIORIDADE_TAREFA")
+    private String prioridadeTarefa;
+
+    @Column(name = "DATA_CRIACAO_TAREFA")
+    private String dataCriacaoTarefa;
+
+}
